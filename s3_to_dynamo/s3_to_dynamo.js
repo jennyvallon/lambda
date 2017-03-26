7890eb4079fc55d4ab2b883f5bb69be8567e14fa
Jenny Vallon <jennyvallon@gmail.com>
2017-03-25 20:29:07 -0400

    added a log object that will collect data from the function and print to log.txt file with or without error. callback(log) log will have log.error obj populated. will print whatever info  has been collected by log up until error thrown. callback(null, log) function is successful and callback will print all information collected by log.

--- s3_to_dynamo/s3_to_dynamo.js
+++ s3_to_dynamo/s3_to_dynamo.js
@@ -6,9 +6,14 @@ var s3=new AWS.S3();
 
 //everytime a text file is uploaded s3_to-dynamo.handler is fired ==> triggered by s3 event added at bucket level
 exports.handler = function(event, context, callback) { //callback function handles errors
-    console.log("Here is the info about the triggering event:");
-    console.log(util.inspect(event, {depth: null,colors:true}));
-    
+    var log={};
+    log.event={};
+    log.event=event;
+    log.err={};
+    log.dynamo={};
+    log.s3={};
+    log.obj={};
+    log.result={};
     var obj={};
     
     obj.key={};//ifo about file name ==> prefix/prefix/key.extension
@@ -16,7 +21,8 @@ exports.handler = function(event, context, callback) { //callback function handl
     obj.key.array=obj.key.full.split("/");
     obj.key.name= obj.key.array[obj.key.array.length-1];
     obj.key.nameMinusExt=obj.key.name.split(".")[0];
-    obj.key.prefix=obj.key.array.pop();
+    obj.key.prefix=obj.key.array;
+    obj.key.prefix.pop();
     obj.bucket=event.Records[0].s3.bucket.name;
     obj.path=obj.bucket+"/"+obj.key.full;
     obj.fileType="";
@@ -26,61 +32,75 @@ exports.handler = function(event, context, callback) { //callback function handl
     // Get file type
     var typeMatch = obj.key.name.match(/\.([^.]*)$/); //break up fileName is any of these characters exist
     if (!typeMatch) {
-        callback("${obj.path} does not have an extension.");
+        log.obj=obj;
+        log.err={message:obj.path+" does not have an extension"};
+        callback(log);
         return;
     }
     
     //Validate that it's text file
     obj.fileType = typeMatch[1];
     if (obj.fileType !== "rtf" && obj.fileType !== "txt" && obj.fileType !== "text") {
-        callback('${obj.path} unsupported file type: '+ obj.fileType);
+        log.obj=obj;
+        log.err={message:obj.path+" unsupported file type: "+obj.fileType};
+        callback(log);
         return;
     }
     
     new Promise(//get file
         function (resolve){
             var params={Bucket:obj.bucket,Key:obj.key.full};
+            log.s3.params=params;
             s3.getObject(params, function(err, dataObj) {
                 if (err){  
-                    callback("There was an error retrieving "+ obj.path +" from s3");
-                    console.log(err);
+                    log.error={message:"There was an error retrieving "+ obj.path +" from s3"};
+                    callback(log);
                     return;
                 } 
-                else resolve(dataObj); 
+                else {
+                    log.s3.result=dataObj;
+                    resolve(dataObj);
+                }
             });
         }
     ).then(function(data){
-        obj.body=data.Body.toString('base64'); //read file
+        obj.body=data.Body.toString('utf-8'); //read file
+        
         new Promise( //push to Dynamo
-            function(){
+            function(resolve){
                 var dynamo=new AWS.DynamoDB();
-                 var params={
+                var params={
+                    ExpressionAttributeNames: {
+                        "#T": obj.key.prefix[1]
+                    }, 
+                    ExpressionAttributeValues: {
+                        ":t": {S:obj.body}
+                    },
                     TableName: obj.bucket,
                     Key:{
-                        'Project':{'S':obj.key.prefix[0]} //SHOULD BE OBJ.PREFIX
+                        'Project':{'S':obj.key.prefix[0]},
+                        'Asset':{'S':obj.key.nameMinusExt}
                     },
-                    UpdateExpression: "SET :a = :b", 
-                    ExpressionAttributeValues: {
-                        ":a":obj.key.prefix[1][obj.nameMinusExt],
-                        ":b": obj.body.toString()
-                    }
-                 };
+                    ReturnValues:"ALL_NEW",
+                    UpdateExpression: "SET #T = :t"
+                };
+                log.dynamo.params=params;
+                log.obj=obj;
                 dynamo.updateItem(params, function (err, result) {
                     if (err){ // an error occurred
-                        callback('There was an error updating ${params.TableName}'); 
-                        console.log(err);
+                        log.err=err;
+                        callback(err); 
                         return;
                     }
                     else{// successful response
-                        console.log('Successfully updated '+params.Key.Asset+' in '+ params.TableName);
-                        console.log('Results of this query');
-                        console.log(result);
-                        resolve();
+                        log.dynamo.result=result;
+                        log.result=result;
+                        resolve(log);
                     }
                 }); 
             }
-        ).then(function(){
-            callback(null,"message");
+        ).then(function(log){
+            callback(null,log);
         });
     });
 };
